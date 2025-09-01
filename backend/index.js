d56612b3f58cf4a652242fb10ac5dd66af78cdfd
shraddhabaghel <131020003+shraddhabaghel@users.noreply.github.com>
2025-09-01 22:11:46 +0530

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const mongoose = require('mongoose')
 const Book = require('./models/Books')
-const PORT = 5001
+const PORT = process.env.PORT || 5001
+const mongo=process.env.MONGO_URI
 const cors=require('cors')
 
 // middleware
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://127.0.0.1:27017/newBook")
+
+mongoose.connect("mongo")
     .then(()=> console.log("Db connected"))
     .catch((err) => console.error(err))
 
